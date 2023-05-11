98eb893a6386eb26ebc9df2750cc3f58a8952d0d
ploopple <mhmda709@gmail.com>
2023-05-11 14:44:44 +0300

    make the signUp button change depends on the signUpData

--- client/src/pages/SignUpPage.tsx
+++ client/src/pages/SignUpPage.tsx
@@ -72,7 +72,9 @@ const SignUpPage = () => {
           </div>
         </div>
         <div>
-          <button disabled={isValidData()} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button 
+            disabled={isValidData()} 
+            className={`flex w-full justify-center rounded-md ${isValidData() ? "bg-indigo-300 cursor-not-allowed" : "bg-indigo-600 hover:bg-indigo-500"}  px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}>Sign in</button>
         </div>
       </div>
     </div>
